1c8dc512e78c97eca4b32c3c252749cd118cfcf9
locknes <locknes91@windowslive.com>
2011-06-12 18:47:17 +0200

    fix my mistake

---
+++ sql/ChaosCore/world_ancitcheat_remove.sql
@@ -0,0 +1,4 @@
+DELETE FROM `command` WHERE `name`='anticheat global';
+DELETE FROM `command` WHERE `name`='anticheat player';
+DELETE FROM `command` WHERE `name`='anticheat handle';
+DELETE FROM `command` WHERE `name`='anticheat delete';
