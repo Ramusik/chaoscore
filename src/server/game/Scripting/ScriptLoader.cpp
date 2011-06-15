822954fb096a15ad5095ea2f757123eec7e933e1
locknes <locknes91@windowslive.com>
2011-06-15 16:15:51 +0200

    Core/Misc Update mmaps

--- src/server/game/Scripting/ScriptLoader.cpp
+++ src/server/game/Scripting/ScriptLoader.cpp
@@ -59,6 +59,7 @@ void AddSC_reload_commandscript();
 void AddSC_tele_commandscript();
 void AddSC_titles_commandscript();
 void AddSC_wp_commandscript();
+void AddSC_pathfinding_commandscript();
 
 // Custom
 void AddSC_city_conquest_boss();
@@ -678,6 +679,7 @@ void AddCommandScripts()
     AddSC_tele_commandscript();
     AddSC_titles_commandscript();
     AddSC_wp_commandscript();
+    AddSC_pathfinding_commandscript();
 }
 
 void AddWorldScripts()
