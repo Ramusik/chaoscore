2126200b09f73bbe42ac5cb6a9466ca855f6dc88
locknes <locknes91@windowslive.com>
2011-06-18 08:43:10 +0200

    Script/ICC fix spell remove in Rotface close #52

--- src/server/scripts/Northrend/IcecrownCitadel/boss_rotface.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/boss_rotface.cpp
@@ -117,6 +117,7 @@ class boss_rotface : public CreatureScript
             {
                 _JustDied();
                 Talk(SAY_DEATH);
+                instance->DoRemoveAurasDueToSpellOnPlayers(SPELL_MUTATED_INFECTION);
                 instance->SetBossState(DATA_ROTFACE, DONE);
                 instance->SetData(DATA_ROTFACE, DONE);
                 if (Creature* professor = Unit::GetCreature(*me, instance->GetData64(DATA_PROFESSOR_PUTRICIDE)))
